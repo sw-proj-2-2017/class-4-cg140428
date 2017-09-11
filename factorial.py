c9eed45015a44fb54467327c770ea8c498f1fbc7
cg140428 <cg140428@kookmin.ac.kr>
2017-09-12 00:08:22 +0900

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,23 +1,20 @@
 #20171661 Lee DaEun
 
-mul = 1
+fac = 1
 run = True
 
 while run:
     num = int(input("Enter your number: "))
-    #입력값이 0일 때
-    if num ==0:
-        print(num, "! = ", mul)
     #입력값이 -1일 때
+    if num > -1:
+        for i in range(1,num+1):
+            fac *= i
+        print(num,"! = ", fac)
+        fac = 1
+    #입력값이 음수인 정수일 때
     elif num == -1:
         print("종료되었습니다.")
         run = False
-    #입력값이 음수인 정수일 때
-    elif num <-1:
-        print("양수인 정수를 입력해주세요.")
     #입력값이 양수인 정수일 때
     else:
-        for i in range(1,num+1):
-            mul *= i
-        print(num,"! = ", mul)
-    mul =1
+        print("양수인 정수를 입력해주세요.")
