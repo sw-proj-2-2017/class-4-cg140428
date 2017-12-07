939881b90c982e3114558ec84d8a3be0ccc0ba2a
cg140428 <cg140428@kookmin.ac.kr>
2017-12-07 21:34:13 +0900

    testHangman edited

--- week14/testHangman.py
+++ week14/testHangman.py
@@ -27,13 +27,19 @@ class TestHangman(unittest.TestCase):
         self.assertEqual(self.h1.remainingLives, 0)
 
     def testCurrentShape(self):
-        self.assertEqual(self.h1.currentShape(), self.h1.text[0])
-        self.assertEqual(self.h1.currentShape(), self.h1.text[1])
-        self.assertEqual(self.h1.currentShape(), self.h1.text[2])
-        self.assertEqual(self.h1.currentShape(), self.h1.text[3])
-        self.assertEqual(self.h1.currentShape(), self.h1.text[4])
-        self.assertEqual(self.h1.currentShape(), self.h1.text[5])
         self.assertEqual(self.h1.currentShape(), self.h1.text[6])
+        self.h1.decreaseLife()
+        self.assertEqual(self.h1.currentShape(), self.h1.text[5])
+        self.h1.decreaseLife()
+        self.assertEqual(self.h1.currentShape(), self.h1.text[4])
+        self.h1.decreaseLife()
+        self.assertEqual(self.h1.currentShape(), self.h1.text[3])
+        self.h1.decreaseLife()
+        self.assertEqual(self.h1.currentShape(), self.h1.text[2])
+        self.h1.decreaseLife()
+        self.assertEqual(self.h1.currentShape(), self.h1.text[1])
+        self.h1.decreaseLife()
+        self.assertEqual(self.h1.currentShape(), self.h1.text[0])
 
 if __name__ == '__main__':
     unittest.main()
