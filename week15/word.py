fa958e1e962b04ccf2957bf08d52f5c797f15338
cg140428 <cg140428@kookmin.ac.kr>
2017-12-09 16:06:51 +0900

    week15 assignment

---
+++ week15/word.py
@@ -0,0 +1,33 @@
+import random
+
+class Word:
+
+    def __init__(self, filename):
+
+        self.words = []
+        f = open(filename, 'r')
+        lines = f.readlines()
+        f.close()
+
+        self.count = 0
+        for line in lines:
+            word = line.rstrip()
+            self.words.append(word)
+            self.count += 1
+
+        print('%d words in DB' % self.count)
+
+
+    def test(self):
+        return 'default'
+
+
+    def randFromDB(self):
+        selected = False
+        r = 0
+        while not selected:
+            r = random.randrange(self.count)
+            length = len(self.words[r])
+            if length > 4:
+                selected = True
+        return self.words[r]
